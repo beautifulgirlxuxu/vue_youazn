af28447ffc3a04849009ab10f435d82ef7ddf5a8
beautifulgirlxuxu <804316947@qq.com>
2018-03-19 21:48:03 +0800

    使用vue-cli开始项目

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <img class="logo" src="./assets/logo.png" alt="">
+        <Menu />
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Menu from './components/menu'
+export default {
+  name: 'App',
+  components: MenuS
+}
+</script>
+
+<style>
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  .logo {
+    width: 60px;
+    height: 60px;
+    float: left;
+  }
+</style>

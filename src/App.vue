b24995506c24594597dd35fa8916275811af217a
beautifulgirlxuxu <804316947@qq.com>
2018-03-23 23:36:16 +0800

    更新了页面

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@
         <span class="title">有赞</span>
         <nav-menu />
       </el-header>
-      <el-main>Main</el-main>
+      <el-main>
+        <Slider />
+        <Shop />
+        <Retail />
+        <MiniProgram />
+      </el-main>
       <el-footer>Footer</el-footer>
     </el-container>
   </div>
@@ -14,15 +19,23 @@
 
 <script>
 import NavMenu from './components/menu'
+import Slider from './components/slider'
+import Shop from './components/shop'
+import Retail from './components/retail'
+import MiniProgram from './components/miniProgram'
 export default {
   name: 'App',
   components: {
-    NavMenu
+    NavMenu,
+    Slider,
+    Shop,
+    Retail,
+    MiniProgram
   }
 }
 </script>
 
-<style>
+<style scoped>
 .el-header, .el-footer {
     background-color: white;
     color: #333;
@@ -30,26 +43,26 @@ export default {
     height: 80px;
   }
   .el-main {
-    background-color: #E9EEF3;
+    // background-color: #E9EEF3;
     color: #333;
     text-align: center;
     line-height: 160px;
   }
   .logo {
     /* display: inline-block; */
-    width: 25px;
-    height: 25px;
+    width: 30px;
+    height: 30px;
     float: left;
-    margin-top: 10px;
+    margin-top: 6px;
     margin-left: 120px;
   }
   .title {
     display: inline-block;
     float: left;
-    padding-top: 5px;
+    padding-top: 8px;
     padding-left: 4px;
     font: bold;
-    font-size: 24px;
+    font-size: 20px;
   }
   .menu {
     display: inline;
@@ -57,5 +70,5 @@ export default {
      padding-top: -50px;
     padding-left: -50px;
   }
-  /* .title */
+
 </style>

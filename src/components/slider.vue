0103aa92058e0ea472fa6df16b68e58ca249cb1a
beautifulgirlxuxu <804316947@qq.com>
2018-03-23 23:40:29 +0800

    走马灯

---
+++ src/components/slider.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="block">
+        <el-carousel trigger="click" height="380px">
+            <el-carousel-item v-for="item in 2" :key="item">
+            <!-- <h3>{{ item }}</h3> -->
+            <h3><img src="../assets/slide1.png" alt=""></h3>
+            <img src="../assets/slide2.jpg" alt="">
+            </el-carousel-item>
+        </el-carousel>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .block {
+        background-color: pink;
+    }
+</style>

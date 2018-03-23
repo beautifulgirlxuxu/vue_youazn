784ba8a867415dff8690cd5e2b51a79b4163ef52
beautifulgirlxuxu <804316947@qq.com>
2018-03-23 23:37:44 +0800

    导航栏菜单

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="menu">
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+ <div class="menu">
+    <el-menu class="el-menu-demo" mode="horizontal" @select="handleSelect">
       <el-menu-item index="1">首页</el-menu-item>
       <el-menu-item index="2">
         <el-dropdown>
@@ -11,14 +11,15 @@
             <el-dropdown-item>黄金糕</el-dropdown-item>
             <el-dropdown-item>狮子头</el-dropdown-item>
             <el-dropdown-item>螺蛳粉</el-dropdown-item>
-            <el-dropdown-item disabled>双皮奶</el-dropdown-item>
-            <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+            <el-dropdown-item>双皮奶</el-dropdown-item>
+            <el-dropdown-item>蚵仔煎</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-menu-item>
       <el-menu-item index="3">有赞云</el-menu-item>
       <el-menu-item index="4">渠道合作</el-menu-item>
       <el-menu-item index="5">
+        <el-dropdown>
           <span>
             服务资源<i class="el-icon-arrow-down el-icon--right"></i>
           </span>
@@ -29,15 +30,16 @@
             <el-dropdown-item disabled>双皮奶</el-dropdown-item>
             <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
           </el-dropdown-menu>
+        </el-dropdown>
       </el-menu-item>
-      <el-menu-item index="6">消费保障</el-menu-item>
-      <el-button type="primary">登录</el-button>
-      <el-menu-item index="8"><el-button type="primary" plain>注册</el-button></el-menu-item>
+      <el-button class="bu" type="primary" plain>登录</el-button>
+      <el-button class="bu" type="primary">注册</el-button>
     </el-menu>
-  </div>
+  </div>    
 </template>
 
 <script>
+
 export default {
     data() {
       return {
@@ -53,16 +55,33 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .el-menu {
     /* float: left;  */
+    margin-top: -10px;
+    margin-left: 150px;
   }
   .el-menu-item {
     width: 120px;
-    /* display: inline-block; */
+    height: 60px;
+    text-align: center;
+    line-height: 60px;
+    display: inline-block;
     float: left;
     list-style: none;
-    padding-left: 10px;
+    /* margin-top: -60px; */
+    /* margin-left: 400px; */
     font-size: 15px;
+  }
+  .el-menu-item :nth-child(2) {
+    /* margin-left: 40px; */
+  }
+  .bu {
+    width: 80px;
+    height: 30px;
+    line-height: 5px;
+    display: inline-block;
+    margin-top: 15px;
+    margin-left: 40px;
   }
 </style>

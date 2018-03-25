36afa02914e728d8bbe223c27bd8df7dc92f00d0
beautifulgirlxuxu <804316947@qq.com>
2018-03-25 23:43:37 +0800

    解决方案模块

---
+++ src/components/solutions.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="solutions">
+        <h1>垂直行业解决方案</h1>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_1.png" alt="">
+            <p>美容美发</p>
+        </div>
+        <div class="block">
+                <img src="https://img.yzcdn.cn/intro/index/pc/solution_2.png" alt="">
+                <p>餐饮外卖</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_3.png" alt="">
+            <p>自媒体</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_4.png" alt="">
+            <p>酒店客栈</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_5.png" alt="">
+            <p>票务卡券</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_6.png" alt="">
+            <p>休闲零食</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_7.png" alt="">
+            <p>生鲜果蔬</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_8.png" alt="">
+            <p>母婴亲子</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_9.png" alt="">
+            <p>教育培训</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_10.png" alt="">
+            <p>护肤美妆</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_11.png" alt="">
+            <p>箱包服饰</p>
+        </div>
+        <div class="block">
+            <img src="https://img.yzcdn.cn/intro/index/pc/solution_12.png" alt="">
+            <p>蛋糕烘焙</p>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.solutions {
+    width: 100%;
+    height: 500px;
+}
+h1 {
+    height: 20px;
+    text-align: center;
+    margin-bottom: 120px;
+}
+.block {
+    float: left;
+    width: 16.66%;
+    height: 125px;
+    text-align: center;
+    margin-bottom: 50px;
+}
+p {
+    width: 100%;
+    height: 22px;
+    margin-top: -120px;
+}
+</style>

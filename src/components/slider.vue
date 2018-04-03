67ab6b983c4f562cf9c1f0be0640a790def84623
beautifulgirlxuxu <804316947@qq.com>
2018-04-03 21:27:54 +0800

    图片轮播

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -1,10 +1,8 @@
 <template>
     <div class="block">
         <el-carousel trigger="click" height="380px">
-            <el-carousel-item v-for="item in 2" :key="item">
-            <!-- <h3>{{ item }}</h3> -->
-            <h3><img src="../assets/slide1.png" alt=""></h3>
-            <img src="../assets/slide2.jpg" alt="">
+            <el-carousel-item v-for="item in items" :key="item">
+                <img :src="item.src" alt="">
             </el-carousel-item>
         </el-carousel>
     </div>
@@ -12,11 +10,19 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            items:[
+                 {src: 'https://img.yzcdn.cn/static/intro/img/newIndex/topbanner1.jpg'},
+                 {src: 'https://img.yzcdn.cn/public_files/2017/10/11/54fbf8f46237a68da47b4f09687f6678.jpg'}
+                //  {src: 'https://img.yzcdn.cn/public_files/2018/02/26/cf3b21c3157240ce8dfb289c3ab21bbe.jpg'}
+            ]
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
     .block {
         background-color: pink;
     }
